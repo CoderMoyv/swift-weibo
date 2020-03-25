990a9caa73272c6cfb61456bce77b47faa1dbc42
陈先生 <chenxiansheng@chenxianshengdeMac.local>
2020-03-25 09:10:13 +0800

    Initial Commit

---
+++ weibo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weibo
+//
+//  Created by 陈先生 on 2020/3/25.
+//  Copyright © 2020 chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
